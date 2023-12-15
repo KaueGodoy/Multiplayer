782cfd7623c0b155b0f5e5c01eda7ea6186d2f0e
Kaue Godoy <105384509+KaueGodoy@users.noreply.github.com>
2023-12-15 19:29:40 -0300

    ScriptableObject

--- Assets/_Scripts/ClearCounter.cs
+++ Assets/_Scripts/ClearCounter.cs
@@ -1,11 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectSO _kitchenObjectSO;
+    [SerializeField] private Transform _counterTopPoint;
+
     public void Interact()
     {
         Debug.Log("Interact");
+        Transform kitchenObjectTransform = Instantiate(_kitchenObjectSO.Prefab, _counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+
+        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().ObjectName);
     }
 }
