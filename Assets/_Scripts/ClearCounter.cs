fb31c4939166a1390f203f9bef8e1a538ee30704
Kaue Godoy <105384509+KaueGodoy@users.noreply.github.com>
2023-12-19 21:42:19 -0300

    CounterInteraction

--- Assets/_Scripts/ClearCounter.cs
+++ Assets/_Scripts/ClearCounter.cs
@@ -2,11 +2,32 @@ using UnityEngine;
 
 public class ClearCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO _kitchenObjectSO;
+    //[SerializeField] private KitchenObjectSO _kitchenObjectSO;
 
     public override void Interact(Player player)
     {
-
+        if (!HasKitchenObject())
+        {
+            if (player.HasKitchenObject())
+            {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // player not carrying anything
+            }
+        }
+        else
+        {
+            if (player.HasKitchenObject())
+            {
+                // player already carrying an object
+            }
+            else
+            {
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 
 }
