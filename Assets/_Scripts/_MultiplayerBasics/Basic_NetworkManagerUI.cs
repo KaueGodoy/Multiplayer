1be8095983a290ada03af2370191791e90228bd9
Kaue Godoy <105384509+KaueGodoy@users.noreply.github.com>
2024-01-15 22:46:04 -0300

    Netcode

---
+++ Assets/_Scripts/_MultiplayerBasics/Basic_NetworkManagerUI.cs
@@ -0,0 +1,27 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Basic_NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] private Button _serverBtn;
+    [SerializeField] private Button _hostBtn;
+    [SerializeField] private Button _clientBtn;
+
+
+    private void Awake()
+    {
+        _serverBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartServer();
+        });
+        _hostBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+        _clientBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+}
