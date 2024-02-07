506db8e31c99c91c2953eeaf5cff3be51ea9ab74
Kaue Godoy <105384509+KaueGodoy@users.noreply.github.com>
2024-02-06 21:33:59 -0300

    SearchAndJoinLobby

--- Assets/_Scripts/_LobbyTutorial/TestLobby.cs
+++ Assets/_Scripts/_LobbyTutorial/TestLobby.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Lobbies;
@@ -10,6 +11,8 @@ public class TestLobby : MonoBehaviour
 
     //public event EventHandler OnLobbyCreated;
 
+    private Lobby _hostLobby;
+    private float _heartbeatTimer;
 
     private async void Start()
     {
@@ -23,6 +26,26 @@ public class TestLobby : MonoBehaviour
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
+    private void Update()
+    {
+        HandleLobbyHeartbeat();
+    }
+
+    private async void HandleLobbyHeartbeat()
+    {
+        if (_hostLobby != null)
+        {
+            _heartbeatTimer -= Time.deltaTime;
+            if (_heartbeatTimer < 0f)
+            {
+                float maxHeartbeatTimer = 15f;
+                _heartbeatTimer = maxHeartbeatTimer;
+
+                await LobbyService.Instance.SendHeartbeatPingAsync(_hostLobby.Id);
+            }
+        }
+    }
+
     public async void CreateLobby()
     {
         try
@@ -30,6 +53,8 @@ public class TestLobby : MonoBehaviour
             string lobbyName = "MyLobby";
             int maxPlayers = 4;
             Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
+
+            _hostLobby = lobby;
 
             Debug.Log("Created lobby! " + lobby.Name + " " + lobby.MaxPlayers);
         }
@@ -39,5 +64,58 @@ public class TestLobby : MonoBehaviour
         }
 
         //OnLobbyCreated?.Invoke(this, EventArgs.Empty);
+    }
+
+    public async void JoinLobby()
+    {
+        try
+        {
+            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
+
+            await Lobbies.Instance.JoinLobbyByIdAsync(queryResponse.Results[0].Id);
+            Debug.Log("Joined the lobby");
+
+            foreach (Lobby lobby in queryResponse.Results)
+            {
+                Debug.Log(lobby.Name + " " + lobby.Id);
+            }
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    public async void ListLobbies()
+    {
+        try
+        {
+            QueryLobbiesOptions queryLobbiesOptions = new QueryLobbiesOptions
+            {
+                Count = 25,
+                Filters = new List<QueryFilter>
+                {
+                    new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT)
+                },
+                Order = new List<QueryOrder>
+                {
+                    new QueryOrder(false, QueryOrder.FieldOptions.Created)
+                }
+
+            };
+
+            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync(queryLobbiesOptions);
+
+            Debug.Log("Lobbies found: " + queryResponse.Results.Count);
+            foreach (Lobby lobby in queryResponse.Results)
+            {
+                Debug.Log(lobby.Name + " " + lobby.MaxPlayers);
+            }
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+
     }
 }
--- Assets/_Scripts/_LobbyTutorial/TestLobbyUI.cs
+++ Assets/_Scripts/_LobbyTutorial/TestLobbyUI.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 public class TestLobbyUI : MonoBehaviour
 {
     [SerializeField] private Button _createLobbyButton;
+    [SerializeField] private Button _searchLobbyButton;
+    [SerializeField] private Button _joinLobbyButton;
     [SerializeField] private TestLobby _testLobby;
 
     private void Awake()
@@ -11,6 +13,14 @@ public class TestLobbyUI : MonoBehaviour
         _createLobbyButton.onClick.AddListener(() =>
         {
             _testLobby.CreateLobby();
+        });
+        _searchLobbyButton.onClick.AddListener(() =>
+        {
+            _testLobby.ListLobbies();
+        });
+        _joinLobbyButton.onClick.AddListener(() =>
+        {
+            _testLobby.JoinLobby();
         });
     }
 }
